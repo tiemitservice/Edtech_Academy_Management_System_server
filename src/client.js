38e42ab620129fb33544daded83774718d3d03fe
KakveyKet <kakveyket408@gmail.com>
2025-07-20 19:26:05 +0700

    init

--- src/client.js
+++ src/client.js
@@ -1,11 +1,13 @@
 // client.js
 const { io } = require("socket.io-client");
-const API_URL = "http://localhost:5000"; // Match backend URL
+// const API_URL = "http://localhost:5000"; // Match backend URL
+const API_URL = "http://188.166.242.109:5000"; // Match backend URL
+
 const socket = io(API_URL, {
   autoConnect: false, // Connect manually in onMounted
   transports: ["websocket"], // Ensure WebSocket transport
   cors: {
-    origin: "http://localhost:5173", // Match your Vue.js frontend
+    origin: "http://188.166.242.109:5173", // Match your Vue.js frontend
   },
 });
 
